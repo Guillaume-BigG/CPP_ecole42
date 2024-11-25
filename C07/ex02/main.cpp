857b08d73ea5ca122e1b025f479706e62e133f64
Guillaume Beaudoin <gbeaudoi@made-f0Cr2s1.clusters.42paris.fr>
2024-11-25 18:12:44 +0100

    added change to C07

--- C07/ex02/main.cpp
+++ C07/ex02/main.cpp
@@ -91,15 +91,18 @@ int main() {
 
     // CHAR TESTING 
     // Testing default constructor
-    std::cout << "Testing Assigment Operator overload" << std::endl;
-    Array<std::string> emptyArray;
-    std::cout << "Empty array size: " << emptyArray.size() << std::endl;
+    std::cout << "Testing empty array of String" << std::endl;
+    Array<std::string> emptystrArray;
+    std::cout << "Empty array size: " << emptystrArray.size() << std::endl;
+    std::cout << std::endl;
 
     // Testing constructor with size parameter
+    std::cout << "Testing array of String" << std::endl;
     Array<std::string> stringArray(5);
     std::cout << "stringArray size: " << stringArray.size() << std::endl;
     for (std::size_t i = 0; i < stringArray.size(); ++i)
         stringArray[i] = "AAA"; 
+    std::cout << std::endl;
 
     // Testing default initialization
     std::cout << "Default initialized elements in stringArray: ";
@@ -109,25 +112,29 @@ int main() {
     std::cout << std::endl;
     std::cout << std::endl;    
     // Testing assignment operator and copy constructor
-    Array<std::string> copyArray(stringArray); // Using copy constructor
-    std::cout << "copyArray : "  << std::endl;	
-    for (std::size_t i = 0; i < copyArray.size(); ++i) 
-		std::cout << copyArray[i] << std::endl;
+    std::cout << "Copyig an array with copy constructor: ";
+    Array<std::string> copystrArray(stringArray); // Using copy constructor
+    std::cout << "copystrArray : "  << std::endl;	
+    for (std::size_t i = 0; i < copystrArray.size(); ++i) 
+		std::cout << copystrArray[i] << std::endl;
+    std::cout << std::endl;      
     std::cout << "Modifying copy[0] to 100 "<< std::endl;
-    copyArray[0] = "BBBB"; // Modify copyArray
-    std::cout << "Modified copyArray[0] = " << copyArray[0] << std::endl;
-    std::cout << "Original stringArray[0] after modifying copyArray: " << stringArray[0] << std::endl;
+    copystrArray[0] = "BBBB"; // Modify copystrArray
+    std::cout << "Modified copystrArray[0] = " << copystrArray[0] << std::endl;
+    std::cout << "Original stringArray[0] after modifying copystrArray: " << stringArray[0] << std::endl;
+    std::cout << std::endl;  
 
     // Testing assignment operator
-    Array<std::string> assignedArray;
-    assignedArray = stringArray; // Using assignment operator
-    std::cout << "assignedArray : "  << std::endl;	
-    for (std::size_t i = 0; i < assignedArray.size(); ++i) 
-		std::cout << assignedArray[i] << std::endl;  
-    std::cout << "Modifying assignedArray[1] to 200 "<< std::endl;
-    assignedArray[1] = "CCC"; // Modify assignedArray
-    std::cout << "Modified assignedArray[1] = " << assignedArray[1] << std::endl;
-    std::cout << "Original stringArray[1] after modifying assignedArray: " << stringArray[1] << std::endl;
+    Array<std::string> assignedStrArray;
+    std::cout << "Copyig an array with Operator Overload: ";
+    assignedStrArray = stringArray; // Using assignment operator
+    std::cout << "assignedStrArray : "  << std::endl;	
+    for (std::size_t i = 0; i < assignedStrArray.size(); ++i) 
+		std::cout << assignedStrArray[i] << std::endl;  
+    std::cout << "Modifying assignedStrArray[1] to 200 "<< std::endl;
+    assignedStrArray[1] = "CCC"; // Modify assignedStrArray
+    std::cout << "Modified assignedStrArray[1] = " << assignedStrArray[1] << std::endl;
+    std::cout << "Original stringArray[1] after modifying assignedStrArray: " << stringArray[1] << std::endl;
   std::cout << std::endl;
     // Testing subscript operator with bounds checking
 
