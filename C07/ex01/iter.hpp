e05239131405e64f8e96816e0717d3d90a92d269
Guillaume Beaudoin <gbeaudoi@made-f0Ar10s7.clusters.42paris.fr>
2024-11-26 12:52:13 +0100

    made change to Iter

--- C07/ex01/iter.hpp
+++ C07/ex01/iter.hpp
@@ -6,8 +6,8 @@
 #include <cstddef>
 #include <string>
 
-template<typename T>
-void	iter(T* array, std::size_t length, void (*f)(T &)) {
+template<typename T, typename K>
+void	iter(T* array, std::size_t length, void (*f)(K &)) {
 	for (std::size_t i = 0; i < length; ++i) 
     {
 		f(array[i]);
